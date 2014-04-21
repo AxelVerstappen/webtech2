5d1ef47ad638a552c66109ac38866b79be968245
AxelVerstappen <axel_verstappen@hotmail.com>
2014-04-21 16:01:01 +0200

    Remove unnecessary faye map

--- timder/faye/client.js
+++
@@ -1,9 +0,0 @@
-var client = new Faye.Client('http://localhost:8000/');
-
-client.subscribe('/messages', function(message) {
-  alert('Got a message: ' + message.text);
-});
-
-client.publish('/scoreboard', {
-  text: 'Hello world'
-});
